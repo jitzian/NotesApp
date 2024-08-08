beef50e50e0d3f19b60145f47d28dd91ba91d466
joNA <jitzian@gmail.com>
2024-08-08 14:02:26 -0400

    tyt jc - Adding constants file to configure DB version and all

---
+++ app/src/main/java/com/test/challenge/toyotaapp/constants/GlobalConstants.kt
@@ -0,0 +1,11 @@
+package com.test.challenge.toyotaapp.constants
+
+class GlobalConstants {
+
+    companion object {
+        //DB
+        const val DB_NAME = "notes_db"
+        const val DB_VERSION = 1
+    }
+
+}
