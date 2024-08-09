5a72319ebbbedbf0cc57adada8d38d6353b32d54
joNA <jitzian@gmail.com>
2024-08-08 21:12:43 -0400

    tyt jc - Wiring up with MainActivity

--- app/src/main/java/com/test/challenge/toyotaapp/MainActivity.kt
+++ app/src/main/java/com/test/challenge/toyotaapp/MainActivity.kt
@@ -5,43 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.test.challenge.toyotaapp.ui.common.CustomAppBar
+import com.test.challenge.toyotaapp.ui.main.ui.MainTasksState
 import com.test.challenge.toyotaapp.ui.theme.ToyotaAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ToyotaAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { CustomAppBar() }
+                ) {
+                    MainTasksState(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(it)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToyotaAppTheme {
-        Greeting("Android")
     }
 }
