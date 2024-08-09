abd7773483e646ee41e7d737628ce9599ae61365
joNA <jitzian@gmail.com>
2024-08-08 20:48:56 -0400

    tyt jc - Adding fallback value to id in TaskEntityDB

--- app/src/main/java/com/test/challenge/toyotaapp/data/db/entities/TaskEntityDB.kt
+++ app/src/main/java/com/test/challenge/toyotaapp/data/db/entities/TaskEntityDB.kt
@@ -9,7 +9,7 @@ import kotlinx.parcelize.Parcelize
 @Entity(tableName = "task")
 data class TaskEntityDB(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     val title: String? = "",
     val content: String? = "",
 ) : Parcelable
