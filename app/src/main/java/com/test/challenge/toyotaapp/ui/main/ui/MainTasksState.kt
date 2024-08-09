9f678b9f159bf16632759246264260ca3e306c03
joNA <jitzian@gmail.com>
2024-08-08 21:12:20 -0400

    tyt jc - Updating color for Floating action button

--- app/src/main/java/com/test/challenge/toyotaapp/ui/main/ui/MainTasksState.kt
+++ app/src/main/java/com/test/challenge/toyotaapp/ui/main/ui/MainTasksState.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +21,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -62,7 +64,9 @@ fun MainTasksState(
                     imageVector = Icons.Default.Add,
                     contentDescription = stringResource(R.string.add_task_TEXT)
                 )
-            }
+            },
+            shape = RoundedCornerShape(50),
+            containerColor = Color(0xFFFF55A3)
         )
     }
 
